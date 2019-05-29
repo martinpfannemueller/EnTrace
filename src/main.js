07a1c6e995eaf76f88c0b5bea2b073fc91fbc4bc
Lukas von Hohnhorst <>
2019-05-29 12:06:15 +0300

    Replaced ChartJS by ApexCharts, added MQTT connec

--- src/main.js
+++ src/main.js
@@ -4,19 +4,19 @@ import BootstrapVue from "bootstrap-vue"
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
-client = new Paho.MQTT.Client("localhost", Number(1883), "mqttCoala");
+// client = new Paho.MQTT.Client("localhost", Number(1883), "mqttCoala");
 
-function onConnect() {
-  console.log("Connected!");
-}
+// function onConnect() {
+//   console.log("Connected!");
+// }
 
-client.onMessageArrived = function (message) {
-  console.log("Message Arrived: " + message.payloadString);
-}
+// client.onMessageArrived = function (message) {
+//   console.log("Message Arrived: " + message.payloadString);
+// }
 
-client.connect({
-  onSuccess: onConnect
-});
+// client.connect({
+//   onSuccess: onConnect
+// });
 
 Vue.use(BootstrapVue);
 var vm = new Vue({
